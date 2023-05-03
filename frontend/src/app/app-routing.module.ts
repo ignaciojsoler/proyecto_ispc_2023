46d677b0b5265fe00a92f8c61308fd60a5064b0c
Juan Fernandez <jaf.79.jf@gmail.com>
2023-05-03 19:58:15 -0300

    ruteo de las paginas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroPrestadorComponent } from './registro-prestador/registro-prestador.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'sobre_nosotros', component: SobreNosotrosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro_prestador', component: RegistroPrestadorComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
