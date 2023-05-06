d3dba6988b239391c32c3d1384fddd73d9eefebf
Ignacio Joaquín Soler <ignaciojsoler@gmail.com>
2023-05-06 15:58:14 -0300

    backend data

---
+++ backend/database.sql
@@ -0,0 +1,7 @@
+/*CREAR LA BASE DE DATOS*/
+El script para la creación de la base de datos se encuentra incorporado 
+en el archivo CrearTablas.py dentro de la carpeta Modelo
+
+Con solo ejecutar el mencionado archivo se crearán todas las tablas 
+necesarias para este proyecto.
+
