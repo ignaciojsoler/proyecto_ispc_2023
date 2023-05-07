da733577007ade28eb58c86c326486e80a9bdb2d
Ignacio Joaquín Soler <ignaciojsoler@gmail.com>
2023-05-07 20:24:03 -0300

    register form component
    faltan validaciones en js

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { LoginComponent } from './login/login.component';
-import { RegistroPrestadorComponent } from './registro-prestador/registro-prestador.component';
 import { HomeComponent } from './home/home.component';
 import { FrecuentesComponent } from './frecuentes/frecuentes.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'sobre_nosotros', component: SobreNosotrosComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'registro_prestador', component: RegistroPrestadorComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: 'home', component: HomeComponent },
   { path: 'frecuentes', component: FrecuentesComponent },
   { path: 'contacto', component: ContactoComponent },
