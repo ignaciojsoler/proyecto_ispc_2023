e2da639ea4e1294cde86c30d6376b5937ed5934b
MDDiBella <dibella.maximiliano@gmail.com>
2023-05-28 18:14:44 -0300

    Formulario reactivo en página de login
    Agrego formualario reactivo en página de login

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+ 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
 
 }
