aef7d567494620ad0db9b8e4baaf23655882942a
luciasoler14 <luciasoler14@gmail.com>
2023-06-02 12:31:04 -0300

    home filtro + categorias

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -8,11 +8,14 @@ import { ProfesionalService } from 'src/app/services/profesional.service';
 })
 export class HomeComponent implements OnInit {
   profesionales: any;
+  categorias: any;
+  categoriaSeleccionada: any;
 
   constructor(private profesionalService: ProfesionalService) {}
 
   ngOnInit(): void {
     this.recuperarProfesionales();
+    // this.filtrarTrabajadores();
   }
 
   recuperarProfesionales() {
@@ -21,4 +24,17 @@ export class HomeComponent implements OnInit {
       this.profesionales = resultado;
     });
   }
+
+  // Filtrado de Categorías:
+  // filtrarTrabajadores() {
+  //   this.profesionalService.obtener().subscribe((resultado) => {
+  //     if(this.categoriaSeleccionada) {
+  //       this.profesionales = this.profesionales.filter(profesional: any => profesional.profesion === this.categoriaSeleccionada);
+  //     } else {
+  //       // Mostrar todos los trabajadores: reiniciar la lista o volver a cargarlos 
+  //       // Ejemplo: this.profesionales = this.servicioObtenerProfesionales.obtenerTodos();
+  //     }
+  //     this.profesionales = resultado;
+  //   });
+  // }
 }
