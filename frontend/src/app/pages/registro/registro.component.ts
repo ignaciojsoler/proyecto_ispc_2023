741ef24cab051c792842fedeb0cc54fbffd2e991
MDDiBella <dibella.maximiliano@gmail.com>
2023-06-04 13:35:26 -0300

    Form-Registro-reactivo-final
    falta algún ajuste en css

--- frontend/src/app/pages/registro/registro.component.ts
+++ frontend/src/app/pages/registro/registro.component.ts
@@ -9,24 +9,18 @@ import { FormControl, MinLengthValidator, Validators} from '@angular/forms';
 })
 export class RegistroComponent {
 
-  name = new FormControl ('', Validators.required);
-  /*surname = new FormControl ('');*/
+  name = new FormControl ('', [Validators.required]);
+  surname = new FormControl ('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl ('', [Validators.required, Validators.minLength(8)]);
   repeatpassword = new FormControl ('', [Validators.required, Validators.minLength(8)]);
   address = new FormControl ('', Validators.required);
-  city = new FormControl ('', Validators.required);
-  province = new FormControl ('', Validators.required);
-  country = new FormControl ('', Validators.required);
- 
-  /*
-  profession = new FormControl ('');
-  priceperhour = new FormControl ('');
-  */
-
-
+  city = new FormControl ('', [Validators.required]);
+  province = new FormControl ('', [Validators.required]);
+  country = new FormControl ('', [Validators.required]);
+  profession = new FormControl ('', [Validators.required]); //ACA ME QUEDE, FALTA PASAR AL HTML
+  priceperhour = new FormControl ('', [Validators.required]);
   
-
   user_type: String | undefined;
 
   constructor(private router: Router) {
